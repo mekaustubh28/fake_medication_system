36f46015ac73dd013b9120b718590800cfc35dff
“Kaustubh <vermakaustubh28@gmail.com>
2023-04-09 09:48:08 +0530

    3rd

--- frontend/ui/js/auth.js
+++ frontend/ui/js/auth.js
@@ -1,7 +1,33 @@
+import Web3 from "web3";
+import { abi, networks } from "../../../build/contracts/ProductVerification.json"
 let signup = document.querySelector(".signup");
 let login = document.querySelector(".login");
 let slider = document.querySelector(".slider");
 let formSection = document.querySelector(".form-section");
+
+async function initWeb3() {
+    if (window.ethereum) {
+        await window.ethereum.enable();
+        const web3 = new Web3(window.ethereum);
+        var accounts = await web3.eth.getAccounts();
+        var networkId = await web3.eth.net.getId();
+        console.log("id = ", networkId, accounts);
+        const account = accounts[0];
+        const contract = new web3.eth.Contract(abi, networks[1680983828029].address);
+        console.log(contract, account);
+        localStorage.setItem("networkId", networkId)
+        localStorage.setItem("account", account)
+        localStorage.setItem("AuthOwnerHash", account)
+        console.log(typeof(localStorage.getItem("isAuthenticated")));
+        document.getElementById('companyID').value = account;
+        // return account;
+    } else {
+        alert("No Ethereum Window Detected!!");
+        // return;
+    }
+}
+
+initWeb3();
 
 signup.addEventListener("click", () => {
     slider.classList.add("moveslider");
